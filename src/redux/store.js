edb2117710a8323b4d7a74aa686416236c381a79
koval-andrey <kovalandrey1234@gmail.com>
2021-05-19 23:30:14 +0300

    HW-06
    2.0

---
+++ src/redux/store.js
@@ -0,0 +1,42 @@
+import { combineReducer } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import phonebookReducer from './phonebook-reducer'
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+
+const contactPersistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
+//const reducer = (state = {}, { type, payload }) => state;
+
+//const store = createStore(reducer);
+export const store = configureStore({
+  reducer: {
+    contacts: persistReducer(contactPersistConfig, phonebookReducer),
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
+});
+export const persistor = persistStore(store);
